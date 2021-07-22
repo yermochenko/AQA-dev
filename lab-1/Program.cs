cf4c5c32693c10877bc345c1ad66d41988ea4682
Sergey Yermochenko <yermochenko@gmail.com>
2021-07-22 22:24:01 +0300

    Add project files.

---
+++ lab-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

6c3c1c0105afee586a82e0f4e5d56ac84fe62f5f
Sergey Yermochenko <yermochenko@gmail.com>
2021-07-25 01:26:44 +0300

    Сделано задание 3 и 4
    Генерация данных выполнена через библиотеку Bogus

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -1,4 +1,5 @@
-﻿using lab1.Model;
+﻿using Bogus;
+using lab1.Model;
 using System;
 using System.Collections.Generic;
 
@@ -8,40 +9,51 @@ namespace lab1
     {
         public static void Main(string[] args)
         {
-            var candidates = new List<Candidate>()
-            {
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Иван",    LastName = "Иванов",       JobTitle = "Программист",   JobDescription = "Junior .Net разработчик",           JobSalary = 500,   DismissalReason = null },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Вера",    LastName = "Александрова", JobTitle = "Тестировщик",   JobDescription = "Senior тестировщик-автоматизатор",  JobSalary = 2_000, DismissalReason = DismissalReason.LackManagementUnderstanding },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Пётр",    LastName = "Петров",       JobTitle = "Администратор", JobDescription = "Senior DBA",                        JobSalary = 5_000, DismissalReason = DismissalReason.LowSalary },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Надежда", LastName = "Николаева",    JobTitle = "Программист",   JobDescription = "Middle JavaScript разработчик",     JobSalary = 950,   DismissalReason = DismissalReason.ProfessionalGrowthLack },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Сидор",   LastName = "Сидоров",      JobTitle = "Тестировщик",   JobDescription = "Middle мануальный тестировщик",     JobSalary = 750,   DismissalReason = DismissalReason.FamilyReasons },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Любовь",  LastName = "Сергеева",     JobTitle = "Аналитик",      JobDescription = "Senior бизнес-аналитик",            JobSalary = 3_500, DismissalReason = DismissalReason.BadTeamMicroclimate },
-                new Candidate() { Id = Guid.NewGuid(), FirstName = "Василий", LastName = "Васильев",     JobTitle = "Программист",   JobDescription = "Senior .Net fullstack разработчик", JobSalary = 4_000, DismissalReason = DismissalReason.Other }
-            };
-            Console.WriteLine("***** КАНДИДАТЫ *****\n");
+            Random random = new Random();
+            var dismissalReason = Enum.GetValues(typeof(DismissalReason));
+            var candidates = new Faker<Candidate>("ru")
+                .RuleFor(candidate => candidate.Id, f => f.Random.Guid())
+                .RuleFor(candidate => candidate.FirstName, f => f.Person.FirstName)
+                .RuleFor(candidate => candidate.LastName, f => f.Person.LastName)
+                .RuleFor(candidate => candidate.JobTitle, f => f.Name.JobTitle())
+                .RuleFor(candidate => candidate.JobDescription, f => f.Name.JobDescriptor())
+                .RuleFor(candidate => candidate.JobSalary, f => f.Random.Int(1, 20) * 100)
+                .RuleFor(candidate => candidate.DismissalReason, f =>
+                {
+                    var index = f.Random.Int(0, dismissalReason.Length);
+                    if (index < dismissalReason.Length)
+                    {
+                        return dismissalReason.GetValue(index);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                })
+                .Generate(random.Next(10, 20));
+            Console.WriteLine($"***** КАНДИДАТЫ (всего {candidates.Count}) *****\n");
             foreach (var candidate in candidates)
             {
                 candidate.Output();
                 Console.WriteLine();
             }
-            var corporationOfGood = new Company() { Name = "Ганди & Co",        Country = "Индия",  City = "Дели",   Address = "ул. Многорукого Шивы, д. 6, корп. 6, оф. 6" };
-            var corporationOfEvil = new Company() { Name = "Иван Грозный Inc.", Country = "Россия", City = "Москва", Address = "ул. Ленина, д. 13" };
-            var employees = new List<Employee>()
-            {
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Иван",     LastName = "Иванов",       JobTitle = "Программист",   JobDescription = "Junior .Net разработчик",           JobSalary = 500,   Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Вера",     LastName = "Александрова", JobTitle = "Тестировщик",   JobDescription = "Senior тестировщик-автоматизатор",  JobSalary = 2_000, Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Пётр",     LastName = "Петров",       JobTitle = "Администратор", JobDescription = "Senior DBA",                        JobSalary = 5_000, Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Надежда",  LastName = "Николаева",    JobTitle = "Программист",   JobDescription = "Middle JavaScript разработчик",     JobSalary = 950,   Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Сидор",    LastName = "Сидоров",      JobTitle = "Тестировщик",   JobDescription = "Middle мануальный тестировщик",     JobSalary = 750,   Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Любовь",   LastName = "Сергеева",     JobTitle = "Аналитик",      JobDescription = "Senior бизнес-аналитик",            JobSalary = 3_500, Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Василий",  LastName = "Васильев",     JobTitle = "Программист",   JobDescription = "Senior .Net fullstack разработчик", JobSalary = 4_000, Company = corporationOfEvil },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Готама",   LastName = "Капур",        JobTitle = "Программист",   JobDescription = "Junior Java разработчик",           JobSalary = 275,   Company = corporationOfGood },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Нитья",    LastName = "Абусария",     JobTitle = "Тестировщик",   JobDescription = "Junior мануальный тестировщик",     JobSalary = 215,   Company = corporationOfGood },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Серендра", LastName = "Джутхани",     JobTitle = "Администратор", JobDescription = "Middle системный инженер",          JobSalary = 425,   Company = corporationOfGood },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Амрита",   LastName = "Бхаттар",      JobTitle = "Тестировщик",   JobDescription = "Middle мануальный тестировщик",     JobSalary = 375,   Company = corporationOfGood },
-                new Employee() { Id = Guid.NewGuid(), FirstName = "Ананта",   LastName = "Варма",        JobTitle = "Программист",   JobDescription = "Middle Java fullstack разработчик", JobSalary = 500,   Company = corporationOfGood }
-            };
-            Console.WriteLine("***** СОТРУДНИКИ *****\n");
+
+            var companies = new Faker<Company>("ru")
+                .RuleFor(company => company.Name, faker => faker.Company.CompanyName())
+                .RuleFor(company => company.Country, faker => faker.Address.Country())
+                .RuleFor(company => company.City, faker => faker.Address.City())
+                .RuleFor(company => company.Address, faker => faker.Address.StreetAddress())
+                .Generate(random.Next(3, 7));
+            var employees = new Faker<Employee>("ru")
+                .RuleFor(employee => employee.Id, f => f.Random.Guid())
+                .RuleFor(employee => employee.FirstName, f => f.Person.FirstName)
+                .RuleFor(employee => employee.LastName, f => f.Person.LastName)
+                .RuleFor(employee => employee.JobTitle, f => f.Name.JobTitle())
+                .RuleFor(employee => employee.JobDescription, f => f.Name.JobDescriptor())
+                .RuleFor(employee => employee.JobSalary, f => f.Random.Int(1, 20) * 100)
+                .RuleFor(employee => employee.Company, f => companies[f.Random.Int(0, companies.Count - 1)])
+                .Generate(random.Next(10, 20));
+            Console.WriteLine($"***** СОТРУДНИКИ (всего {employees.Count}) *****\n");
             foreach (var employee in employees)
             {
                 employee.Output();
